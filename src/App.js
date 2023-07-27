df9d4db59dd78e42aedd3d0caf208a82262b4884
harsh kumar <harsh.kumar@sparxitsolutions.com>
2023-07-27 12:52:43 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 // Import everything needed to use the `useQuery` hook
-import { useQuery, gql } from '@apollo/client';
-import Displaylocation from './Displaylocation';
+import { useQuery, gql } from "@apollo/client";
+import Displaylocation from "./Displaylocation";
 export default function App() {
-
-  
   return (
     <div>
       <h2>My first Apollo app 🚀</h2>
-      <Displaylocation/>
+      <p>m in app component.</p>
+      <Displaylocation />
     </div>
   );
 }
